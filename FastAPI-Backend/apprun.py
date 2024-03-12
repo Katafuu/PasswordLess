6227e71cc85090e502f8645a185919663d45aa80
🤨 ⌛️ <galaxyfriend21@gmail.com>
2024-03-12 16:08:43 +0400

    venv fixed w/only required packages and dynamic fastapi hosting added with os

--- FastAPI-Backend/apprun.py
+++ FastAPI-Backend/apprun.py
@@ -1,7 +1,9 @@
-import uvicorn
+import uvicorn, os
 
-keydir = "D:/Projects/Passwordless/Website/FastAPI-Backend/localkey.pem"
-certdir = "D:/Projects/Passwordless/Website/FastAPI-Backend/localcert.pem"
+currentdir = os.path.dirname(os.path.realpath(__file__))
+keydir = currentdir+"/localkey.pem"
+certdir = currentdir+"/localcert.pem"
+
 if __name__ == "__main__":
    uvicorn.run("main:app", host="0.0.0.0", ssl_certfile=certdir, ssl_keyfile=keydir, reload=True)
 # uvicorn main:app --host 0.0.0.0 --ssl-keyfile D:/Projects/Passwordless/Website/FastAPI-Backend/localkey.pem --ssl-certfile D:/Projects/Passwordless/Website/FastAPI-Backend/localcert.pemcert.pem 
