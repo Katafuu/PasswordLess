0620117d301910ce8b056f46e23b5d1bb36ff8d8
🤨 ⌛️ <galaxyfriend21@gmail.com>
2024-04-17 10:06:50 +0400

    finding fields working

--- Extension/background.js
+++ Extension/background.js
@@ -1,16 +1,19 @@
 console.log("running")
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
+  function(request, sender, sendResponse){
     if (request.rq == 'token') {
+      console.log("reached")
       chrome.cookies.get({ url: 'https://passwordless.duckdns.org/', name: 'token' },
       function (cookie) {
         if (cookie) {
           console.log(cookie);
-          sendResponse(cookie.value)
+          sendResponse({token:cookie.value});
         }
         else {
           console.log('Can\'t get cookie!');
-        }
+          sendResponse({token:"fail"});
+        };
+        return true;
       });
     }
   }
--- Extension/content.js
+++ Extension/content.js
@@ -1,45 +1,51 @@
-
-
 function checkField(name){
-  const field = document.querySelector('input[type*='+name+']'); // the * indicates it is looking for one instance of the args[0] value within the query. https://www.w3.org/TR/selectors/#attribute-substrings
-  console.log('field found first')
+  let field = document.querySelector('input[type*='+name+']'); // the * indicates it is looking for one instance of the args[0] value within the query. https://www.w3.org/TR/selectors/#attribute-substrings
+  console.log('checking Type '+name)
   if (!field){
-    console.log("reached field missing")
-    field = document.getElementById(name); // extra checks added to maximise chances of finding it
+    console.log("checking ID: "+name)
+    field = document.querySelector('input[id*='+name+']'); // extra checks added to maximise chances of finding it
     if (!field) {
-      field = document.getElementsByName(name);
+      console.log('checking Name '+name)
+      field = document.querySelector('input[name*='+name+']');
+      if (!field) {
+        console.log(name+ " FIELD NOT FOUND")
+        console.log('-----')
+        return null
+      }
+      else {
+        console.log(name+ " FIELD FOUND: "+field)
+        return field
+      }
     } 
     else{
-      console.log(field)
+      console.log(name+ " FIELD FOUND: "+field)
       return field
     };
   } 
   else {
-    console.log(field)
+    console.log(name+ " FIELD FOUND: "+field)
     return field
   };
 };
 
-function findField(standardNames) {
-  const field = null;
-  const count = 0;
-  while(!field && count < length(standardNames)) {
+function findFieldFromNames(standardNames) {
+  var field = null;
+  var count = 0;
+  while(!field && count < standardNames.length) {
     field = checkField(standardNames[count]);
     count++;
   };
   return field
 };
 
-const resptest = chrome.runtime.sendMessage({rq:'token'});
-
 function main() {
   chrome.runtime.sendMessage({rq:'token'})
   .then(function(token) {
     console.log(token); // json error. idk how to process response
-    const pwd_field = findField(['password','password-field','pwd','user_password']);
+    const pwd_field = findFieldFromNames(['password','password-field','pwd','user_password']);
     console.log(pwd_field)
     if (pwd_field) {
-      const url = document.baseURI;
+      const url = window.location.origin;
       console.log(url)
       const options = {
         method: 'GET',
@@ -48,19 +54,19 @@ function main() {
         }
       }; 
       console.log(options)
-      fetch("https://passwordless.duckdns.org/creds/getCred?siteurl="+url,options)
-      .then(data => data.json)
+      fetch("https://passwordless.duckdns.org:8000/creds/getCred?siteurl="+url,options)
       .then(function(data){
-        if (data) {
-          const email_field = findField(['email']);
-          const uname_field = findField(['username','name']);
-          pwd_field.innerText = data.password;
-          uname_field.innerText = data.username;
-          email_field.innerText = data.email;
+        if (data.status == 200) {
+          data = data.json()
+          const email_field = findFieldFromNames(['email','logEmail']);
+          const uname_field = findFieldFromNames(['signName','username','name']);
+          pwd_field.value = data.password;;
+          uname_field.value = data.username;
+          email_field.value = data.email;;
         } 
         else {
           // to add popup to save code here w/event listener
-          console.log("No saved credential found: "+data);
+          alert("No saved credential found");
         };
       });
     }
